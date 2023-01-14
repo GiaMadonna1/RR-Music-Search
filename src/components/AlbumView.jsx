b32d683ad00526a9805315073f702090e9d91829
Gia Madonna <giamadonna1@gmail.com>
2023-01-14 12:22:14 -0800

    About halfway thru codeAlong

--- src/components/AlbumView.jsx
+++ src/components/AlbumView.jsx
@@ -1,11 +1,32 @@
 import { useState, useEffect } from "react";
 
 function AlbumView() {
+    const { id } = useParams()
     const [ albumData, setAlbumData ] = ([])
+
+    useEffect(()=>{
+        const API_URL = `http://localhost:3001/song/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+
+    const renderSongs = justSongs.map((song, i ) =>{
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
 
     return (
         <div>
-            <p>Album Data Goes Here!</p>
+            {renderSongs}
         </div>
     )
 }
--- src/components/ArtistView.jsx
+++ src/components/ArtistView.jsx
@@ -1,16 +1,39 @@
+import { render } from "@testing-library/react";
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
-    return (
-        <div>
-            <h2> The id passed was: {id}</h2>
-            <p> Artist Data goes here!</p>
-        </div>
-    )
+    useEffect(()=> {
+        const API_URL = 'https://localhost3001/album/${id}'
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            console.log(resData)
+        }
+        fetchData()
+    }, [id])
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i)=> {
+        return (
+            <div key={i}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+        return (
+            <div>
+                <h2> The id passed was: {id} </h2>
+                <p> Artist Data goes here!</p>
+                {renderAlbums}
+            </div>
+        )
 }      
 
 export default ArtistView
