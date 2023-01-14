365e05b2414e8ddf92b834ca031d452467660120
Gia Madonna <giamadonna1@gmail.com>
2023-01-14 11:59:49 -0800

    Finished Homework. Really Cool

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import {useEffect, useState } from 'react'
+import {Fragment, useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
-import { DataContext } from './context/DataContext'
+import SearchBar from './components/Searchbar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -33,11 +35,19 @@ function App() {
 
 	return (
 		<div>
-			<SearchBar handleSearch = {handleSearch}/>
 			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+                <Router>
+                    <Routes>
+                        <Route path='/' element= {
+                            <Fragment>
+                                <SearchBar handleSearch = {handleSearch}/>
+                                <Gallery data={data} />
+                            </Fragment>
+                        } />
+                        <Route path='/album/:id' element={<AlbumView />} />
+                        <Route path='/artist/:id' element={<ArtistView />} />
+                    </Routes>
+                </Router>
 		</div>
   	);
 }
