cda4b802bf6abd23ef2c095781b631718ad1cc38
Gia Madonna <giamadonna1@gmail.com>
2023-01-13 23:51:53 -0800

    Fully Functional Now

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
-import {useEffect, useState, useRef } from 'react'
+import { useState, useRef } from 'react'
 import Gallery from './components/Gallery'
-import SearchBar from './components/Searchbar'
+import SearchBar from './components/SearchBar'
 import { DataContext } from './context/DataContext'
-import { SearchContext } from './context/SearchContext' 
+import { SearchContext } from './context/SearchContext'
 
-function App() {
+function App(){
 	let [message, setMessage] = useState('Search for Music!')
 	let [data, setData] = useState([])
 	let searchInput = useRef('')
 
 	const API_URL = 'https://itunes.apple.com/search?term='
-
+	
 	const handleSearch = (e, term) => {
 		e.preventDefault()
-			const fetchData = async () => {
-				document.title = `${term} Music`
-				const response = await fetch(API_URL + term)
-				const resData = await response.json()
-				if (resData.results.length > 0) {
-					return setData(resData.results)
-				} else {
-					return setMessage('Not Found.')
-				}
+		// Fetch Data
+		const fetchData = async () => {
+			document.title = `${term} Music`
+			const response = await fetch(API_URL + term)
+			const resData = await response.json()
+			if (resData.results.length > 0) {
+				// Set State and Context value
+				return setData(resData.results)
+			} else {
+				return setMessage('Not Found')
 			}
-			fetchData()
 		}
-		
-	}	
-	
+		fetchData()
+	}
+
 	return (
-		<div className='App'>
-			<SearchContext.Provider value ={{
+		<div>
+			<SearchContext.Provider value={{
 				term: searchInput,
 				handleSearch: handleSearch
 			}}>
